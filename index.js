7c3b6d00b46dad677440d3e012db0020af071cfc
paul kabuu <kongkabuu9@gmail.com>
2023-03-13 22:25:43 +0300

    completed assingmnet

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()* 10)+2;
+
+let num3= 104;
+let num4 = 5;
+let mod = num3%num4;
+
+let max = Math.max(12,13,14,15,20);
